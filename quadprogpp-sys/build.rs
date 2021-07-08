25f5722931a9e2fd35832042d9d8b493ca9f3112
Mitsutoshi Aoe <me@maoe.name>
2021-07-08 12:13:43 +0900

    sys: Add trace-solver feature flag

--- quadprogpp-sys/build.rs
+++ quadprogpp-sys/build.rs
@@ -1,9 +1,13 @@
 fn main() {
-    cxx_build::bridge("src/lib.rs")
+    let mut build = cxx_build::bridge("src/lib.rs");
+    build
         .file("upstream/src/Array.cc")
         .file("upstream/src/QuadProg++.cc")
         .include("upstream/src")
         .flag("-std=c++14")
-        .flag("-Wno-extra")
-        .compile("libquadprog.a");
+        .flag("-Wno-extra");
+    if cfg!(features = "trace-solver") {
+        build.define("TRACE_SOLVER", "1");
+    }
+    build.compile("libquadprog.a");
 }
