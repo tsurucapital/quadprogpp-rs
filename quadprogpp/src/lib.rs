3cb9df3ac2c36faf7b8a91b3017be2abf3db5780
Mitsutoshi Aoe <me@maoe.name>
2021-07-09 09:39:02 +0900

    Introduce the Constraints struct with helper functions
    Constraints::NONE is useful to remove the need of type annotation.

--- quadprogpp/src/lib.rs
+++ quadprogpp/src/lib.rs
@@ -1,6 +1,6 @@
 use std::ptr;
 
-use ndarray::{ArrayBase, Ix1, Ix2, RawData};
+use ndarray::{ArrayBase, Ix1, Ix2, OwnedRepr, RawData};
 use quadprogpp_sys as sys;
 use thiserror::Error;
 
@@ -32,6 +32,58 @@ impl From<sys::Exception> for Error {
     }
 }
 
+/// Equality/inequality constraints.
+///
+/// The coefficients matrix should be an NxP matric where N is the number of variables and P is
+/// the number of constraints. The constants should be a vector of length P.
+pub struct Constraints<S: RawData<Elem = f64>, S0: RawData<Elem = f64>> {
+    /// Coefficient part of the constraints
+    coeffs: ArrayBase<S, Ix2>,
+    /// Constant terms of the constraints
+    consts: ArrayBase<S0, Ix1>,
+}
+
+impl Constraints<OwnedRepr<f64>, OwnedRepr<f64>> {
+    /// Empty constraints
+    ///
+    /// This makes type annotation unnecessary when passing empty constraints to [`solve`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ndarray::array;
+    /// # use quadprogpp::*;
+    /// let ce = Constraints::NONE;
+    /// assert!(ce.is_none());
+    /// ```
+    pub const NONE: Option<Self> = None;
+}
+
+impl<S, S0> Constraints<S, S0>
+where
+    S: RawData<Elem = f64>,
+    S0: RawData<Elem = f64>,
+{
+    /// Creates a new set of constraints
+    pub fn new(coeffs: ArrayBase<S, Ix2>, consts: ArrayBase<S0, Ix1>) -> Self {
+        Self { coeffs, consts }
+    }
+
+    /// Create a new set of constraints wrapped with Option.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ndarray::array;
+    /// # use quadprogpp::*;
+    /// let ce = Constraints::some(array![[1.], [1.]], array![-3.]);
+    /// assert!(ce.is_some());
+    /// ```
+    pub fn some(coeffs: ArrayBase<S, Ix2>, consts: ArrayBase<S0, Ix1>) -> Option<Self> {
+        Some(Self::new(coeffs, consts))
+    }
+}
+
 macro_rules! assert_size {
     ($term:expr, $expected:expr, $actual:expr) => {
         if $expected != $actual {
@@ -45,9 +97,10 @@ macro_rules! assert_size {
 }
 
 pub fn solve<G, G0, CE, CE0, CI, CI0>(
-    (g, g0): (ArrayBase<G, Ix2>, ArrayBase<G0, Ix1>),
-    ce: Option<(ArrayBase<CE, Ix2>, ArrayBase<CE0, Ix1>)>,
-    ci: Option<(ArrayBase<CI, Ix2>, ArrayBase<CI0, Ix1>)>,
+    g: ArrayBase<G, Ix2>,
+    g0: ArrayBase<G0, Ix1>,
+    ce: Option<Constraints<CE, CE0>>,
+    ci: Option<Constraints<CI, CI0>>,
 ) -> Result<(Vec<f64>, f64)>
 where
     G: RawData<Elem = f64>,
@@ -64,13 +117,13 @@ where
     assert_size!(g0.dim(), g_n, g0_n);
     let mut g0 = unsafe { sys::new_vector_from_ptr(g0.as_ptr(), g0_n as u32) };
     let (ce, ce0) = match ce {
-        Some((ce, ce0)) => {
-            let (ce_n, ce_m) = ce.dim();
+        Some(Constraints { coeffs, consts }) => {
+            let (ce_n, ce_m) = coeffs.dim();
             assert_size!(ce.dim(), g_n, ce_n);
-            let ce = unsafe { sys::new_matrix_from_ptr(ce.as_ptr(), ce_n as u32, ce_m as u32) };
-            let ce0_n = ce0.dim();
+            let ce = unsafe { sys::new_matrix_from_ptr(coeffs.as_ptr(), ce_n as u32, ce_m as u32) };
+            let ce0_n = consts.dim();
             assert_size!(ce0.dim(), ce0_n, ce_m);
-            let ce0 = unsafe { sys::new_vector_from_ptr(ce0.as_ptr(), ce0_n as u32) };
+            let ce0 = unsafe { sys::new_vector_from_ptr(consts.as_ptr(), ce0_n as u32) };
             (ce, ce0)
         }
         None => {
@@ -80,13 +133,13 @@ where
         }
     };
     let (ci, ci0) = match ci {
-        Some((ci, ci0)) => {
-            let (ci_n, ci_m) = ci.dim();
+        Some(Constraints { coeffs, consts }) => {
+            let (ci_n, ci_m) = coeffs.dim();
             assert_size!(ci.dim(), g_n, ci_n);
-            let ci = unsafe { sys::new_matrix_from_ptr(ci.as_ptr(), ci_n as u32, ci_m as u32) };
-            let ci0_n = ci0.dim();
+            let ci = unsafe { sys::new_matrix_from_ptr(coeffs.as_ptr(), ci_n as u32, ci_m as u32) };
+            let ci0_n = consts.dim();
             assert_size!(ci0.dim(), ci0_n, ci_m);
-            let ci0 = unsafe { sys::new_vector_from_ptr(ci0.as_ptr(), ci0_n as u32) };
+            let ci0 = unsafe { sys::new_vector_from_ptr(consts.as_ptr(), ci0_n as u32) };
             (ci, ci0)
         }
         None => {
@@ -111,7 +164,7 @@ where
 #[cfg(test)]
 mod tests {
     use approx::{assert_abs_diff_eq, assert_ulps_eq};
-    use ndarray::{array, Array, Array2, ArrayView1, ArrayView2};
+    use ndarray::{array, Array, Array2};
 
     use super::*;
 
@@ -135,7 +188,12 @@ mod tests {
             [0.0, 1.0, 1.0],
         ];
         let ci0 = array![0.0, -2.0, 0.0];
-        let (x, best) = solve((g, g0), Some((ce, ce0)), Some((ci, ci0)))?;
+        let (x, best) = solve(
+            g,
+            g0,
+            Constraints::some(ce, ce0),
+            Constraints::some(ci, ci0),
+        )?;
         assert_ulps_eq!(best, 12.0);
         assert_ulps_eq!(x[0], 1.0);
         assert_ulps_eq!(x[1], 2.0);
@@ -155,7 +213,13 @@ mod tests {
             [0.0, 0.0, 1.0,  0.0]
         ];
         let ci0 = array![0.0, 0.0, 0.0, 10.0];
-        let (x, best) = solve((g, g0), Some((ce, ce0)), Some((ci, ci0))).unwrap();
+        let (x, best) = solve(
+            g,
+            g0,
+            Constraints::some(ce, ce0),
+            Constraints::some(ci, ci0),
+        )
+        .unwrap();
         assert_ulps_eq!(best, 6.4);
         assert_ulps_eq!(x[0], 0.0);
         assert_ulps_eq!(x[1], 2.0);
@@ -171,14 +235,13 @@ mod tests {
             [0.0, 2.0],
         ];
         let g0 = array![-4.0, -8.0];
-        let ce: Option<(ArrayView2<_>, ArrayView1<_>)> = None;
         #[rustfmt::skip]
         let ci = array![
             [1.0, 0.0, -1.0],
             [0.0, 1.0, -2.0],
         ];
         let ci0 = array![0.0, 0.0, 2.0];
-        let (answer, _) = solve((g, g0), ce, Some((ci, ci0)))?;
+        let (answer, _) = solve(g, g0, Constraints::NONE, Constraints::some(ci, ci0))?;
         assert_abs_diff_eq!(answer[0], 2.0 / 9.0, epsilon = 1e-12);
         assert_abs_diff_eq!(answer[1], 8.0 / 9.0, epsilon = 1e-12);
         Ok(())
@@ -204,7 +267,12 @@ mod tests {
             [0.0, -1.0 / 3.0,  1.0 / 3.0]
         ];
         let ci0 = array![0.0, 0.0, 2.0];
-        let (answer, _) = solve((g, g0), Some((ce, ce0)), Some((ci, ci0)))?;
+        let (answer, _) = solve(
+            g,
+            g0,
+            Constraints::some(ce, ce0),
+            Constraints::some(ci, ci0),
+        )?;
         assert_ulps_eq!(answer[0], 2.0 / 9.0, epsilon = 1e-5);
         assert_abs_diff_eq!(answer[1], 10.0 / 9.0, epsilon = 1e-5);
         assert_abs_diff_eq!(answer[2], -14.0 / 9.0, epsilon = 1e-5);
